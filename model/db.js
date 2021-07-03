5d30d5438e5fd19919cfdbc85fc23ff02890f464
vetallatka <latka.vitalik@yandex.ru>
2021-07-03 18:20:05 +0200

    made and fixed lints

--- model/db.js
+++ model/db.js
@@ -3,30 +3,30 @@ require('dotenv').config()
 const uriDb = process.env.URI_DB
 
 const db = mongoose.connect(uriDb, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-    useCreateIndex: true,
-    useFindAndModify: false,
-    poolSize: 5,
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true,
+  useFindAndModify: false,
+  poolSize: 5,
 })
 
 mongoose.connection.on('connected', (err) => {
-    console.log(`Mongoose connected`)
+  console.log('Mongoose connected')
 })
 
 mongoose.connection.on('error', (err) => {
-    console.log(`Mongoose connection error: ${err.message}`)
+  console.log(`Mongoose connection error: ${err.message}`)
 })
 
 mongoose.connection.on('disconnected', (err) => {
-    console.log(`Mongoose disconnected`)
+  console.log('Mongoose disconnected')
 })
 
 process.on('SIGINT', () => {
-    mongoose.connection.close(() => {
-        console.log('Connection for DB disconnected and app terminated')
-        process.exit(1)
-    })
+  mongoose.connection.close(() => {
+    console.log('Connection for DB disconnected and app terminated')
+    process.exit(1)
+  })
 })
 
 module.exports = db
--- model/schema.js
+++ model/schema.js
@@ -2,20 +2,20 @@ const mongoose = require('mongoose')
 const { Schema, model } = mongoose
 
 const contact = new Schema({
-    name: {
-        type: String,
-        required: [true, 'Set name for contact'],
-    },
-    email: {
-        type: String,
-    },
-    phone: {
-        type: String,
-    },
-    favorite: {
-        type: Boolean,
-        default: false,
-    },
+  name: {
+    type: String,
+    required: [true, 'Set name for contact'],
+  },
+  email: {
+    type: String,
+  },
+  phone: {
+    type: String,
+  },
+  favorite: {
+    type: Boolean,
+    default: false,
+  },
 })
 
 const Contacts = model('contact', contact)
